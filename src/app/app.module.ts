234d515af47d033237203a35277f798401677df3
anishpabby <anishpabby@gmail.com>
2020-04-19 18:37:26 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { RulesComponent } from './rules/rules.component';
+import { GameComponent } from './game/game.component';
+
+const approutes : Routes = [
+  {path : 'rules', component : RulesComponent,pathMatch : 'full'},
+  {path : '', component : GameComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RulesComponent,
+    GameComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(approutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
